4bbe09aead1326f70efeb0ad07d991115ff39dfd
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-05-08 18:32:08 +0300

    Initialize Redux store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+
+export default store;
